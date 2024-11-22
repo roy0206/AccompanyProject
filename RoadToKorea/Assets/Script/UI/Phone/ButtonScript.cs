370e6beec5fbf3bec96938bc971b92d87712dfda
laeswo <129704273+laeswo@users.noreply.github.com>
2024-11-22 23:18:49 +0900

    dropbox
    드롭박스까지 제작완료

---
+++ RoadToKorea/Assets/Script/UI/Phone/ButtonScript.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+   
+    public List<GameObject> panel;
+    public List<GameObject> buttons;
+
+    public Dropdown Language;
+    public enum Language_select
+    {
+        korean,
+        english,
+        japanese
+    };
+    public Language_select Language_state;
+
+    public enum panel_state
+    {
+        phone_screen,
+        setting_screen,
+        note_screen,
+        task_screen,
+        map_screen,
+        bag_screen,
+        no_screen,
+    };
+
+    public panel_state screen_state;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        screen_state = panel_state.no_screen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //언어 설정
+        switch (Language.value)
+        {
+            case 0 : 
+                Language_state = Language_select.korean;
+                break;
+            case 1 :
+                Language_state = Language_select.english;
+                break;
+            case 2 : 
+                Language_state = Language_select.japanese;
+                break;
+        }
+       Debug.Log(Language_state);
+       //화면상태
+       switch (screen_state)
+       {
+           case panel_state.phone_screen:
+               buttons[0].SetActive(false);
+               panel[0].SetActive(true);
+               panel[1].SetActive(false);
+               break;
+           case panel_state.setting_screen:
+               buttons[0].SetActive(false);
+               panel[0].SetActive(false);
+               panel[1].SetActive(true);
+               break;
+          case panel_state.no_screen:
+               buttons[0].SetActive(true);
+               panel[0].SetActive(false);
+               panel[1].SetActive(false);
+              break;
+       }
+
+    }
+
+    public void OnPhoneScreen()
+    {
+        screen_state = panel_state.phone_screen;
+    }
+    public void OnClickSettingButtons()
+    {
+        screen_state = panel_state.setting_screen;
+    }
+}
