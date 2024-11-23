1f84b9fb7d70249b8e63c632ed76c1b807237628
roy0206 <swimkinig_ryu@naver.com>
2024-11-23 23:27:54 +0900

    카드 테그 기능

--- RoadToKorea/Assets/Script/System/TagFunction.cs
+++ RoadToKorea/Assets/Script/System/TagFunction.cs
@@ -1,28 +1,53 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TagFunction : MonoBehaviour
 {
     [SerializeField] Transform card;
     [SerializeField] Gateway destination;
+    [SerializeField] Image img;
     float tagTime;
+    Vector2 pos;
+    private void Start()
+    {
+        pos = card.transform.position;
+        tagTime = 0;
+    }
 
     private void Update()
     {
-        Collider2D collider = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), 1, LayerMask.GetMask("CardTag"));
+        if (Input.touchCount == 0)
+        {
+            card.transform.position = (Vector3)pos + new Vector3(0, 0, 1);
+            return;
+        }
+
+        Vector2 touchPos = Input.GetTouch(0).position;
+        Collider2D collider = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(touchPos), 1, LayerMask.GetMask("Card"));
         if (collider)
+            card.transform.position = Camera.main.ScreenToWorldPoint(touchPos) + new Vector3(0,0,1);
+        else
+            card.transform.position = (Vector3)pos + new Vector3(0, 0, 1);
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        print(tagTime);
+        if (other)
         {
             tagTime += Time.deltaTime;
-            if(tagTime > 1)
+
+            if (tagTime > 1)
             {
                 tagTime = -99999;
                 StartCoroutine(OnTagged());
             }
         }
-        else
-        {
-            tagTime = 0;
-        }
+        else tagTime = 0;
+        img.fillAmount = tagTime;
     }
 
     IEnumerator OnTagged()
